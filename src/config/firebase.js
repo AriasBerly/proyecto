dc55c1abdd1812fec50819910626a7cc702a6c36
AlvaroLe6 <alvaro_147@hotmail.es>
2024-05-06 18:40:37 -0400

    06/05/2024

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,14 +1,15 @@
 import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore';
 
-  const firebaseConfig = {
-    apiKey: "AIzaSyAfeH05JqpuOPjcNulWrfSegqx_sVlPHzY",
-    authDomain: "contabilidad-e77a2.firebaseapp.com",
-    projectId: "contabilidad-e77a2",
-    storageBucket: "contabilidad-e77a2.appspot.com",
-    messagingSenderId: "172175111817",
-    appId: "1:172175111817:web:37fe304cd55b68fabaf858"
-  };  
+const firebaseConfig = {
+  apiKey: "AIzaSyBYFJgfipTHw-MUbp4B4CLvRbf1C0c6CFQ",
+  authDomain: "seguimiento-c8de4.firebaseapp.com",
+  projectId: "seguimiento-c8de4",
+  storageBucket: "seguimiento-c8de4.appspot.com",
+  messagingSenderId: "641100302195",
+  appId: "1:641100302195:web:3fe50d233077800914e706"
+};
+  
 
 export const firebaseApp = initializeApp(firebaseConfig)
 export const db = getFirestore(firebaseApp);
